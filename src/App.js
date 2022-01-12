45bf61202856c22b3abc9d70669dd6f833d7e15f
Michal Andronik <michal.androni@gmail.com>
2022-01-12 11:16:00 +0100

    Add hovers and links for 'FeatureBox' elements

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPage';
+import CustomerSupport from './components/views/CustomerSupport/CustomerSupport.js';
+import MemberDiscount from './components/views/MemberDiscount/MemberDiscount.js';
+import MoneyBack from './components/views/MoneyBack/MoneyBack';
 
 const App = () => (
   <Provider store={store}>
@@ -19,6 +22,9 @@ const App = () => (
           <Route exact path={'/'} component={Homepage} />
           <Route exact path={'/shop/:categoryId'} component={ProductList} />
           <Route exact path={'/product/:productId'} component={ProductPage} />
+          <Route exact path={'/customerSupport'} component={CustomerSupport} />
+          <Route exact path={'/MoneyBack'} component={MoneyBack} />
+          <Route exact path={'/MemberDiscount'} component={MemberDiscount} />
         </Switch>
       </MainLayout>
     </BrowserRouter>
---
+++ src/components/views/CustomerSupport/CustomerSupport.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import styles from './CustomerSupport.module.scss';
+
+const CustomerSupport = () => <div className={styles.root}>Customer Support</div>;
+
+// ProductList.propTypes = {};
+
+export default CustomerSupport;
