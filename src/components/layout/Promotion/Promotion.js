6eca08bafe984fd14925c9cf2807c40c94a404d4
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-01-25 16:01:07 +0100

    Add WDP220101-26 - RWD Promotions

--- src/components/layout/Promotion/Promotion.js
+++ src/components/layout/Promotion/Promotion.js
@@ -5,12 +5,12 @@ const Promotion = () => (
   <div className='container'>
     <div className={styles.root}>
       <div className='row align-items-center'>
-        <div className={`col-6 ${styles.noPadding} `}>
+        <div className={`col-lg-6 col-sm-12 ${styles.noPadding} `}>
           <div className={`${styles.photo} ${styles.vertical}`}>
             <img src='/images/promotion/fotel.jpg' alt='armchair' />
           </div>
         </div>
-        <div className={`col-6 ${styles.noPadding} `}>
+        <div className={`col-lg-6 col-sm-12 ${styles.noPadding} `}>
           <div
             className={`${styles.photo} ${styles.horizontal} ${styles.paddingBottom}`}
           >
