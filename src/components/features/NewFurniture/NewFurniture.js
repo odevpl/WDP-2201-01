5e014053d03829d964ad80f41b97ba4f2320b9c3
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-01-27 11:56:13 +0100

    Add WDP220101-19 - New Furnitures - animated category changing

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -9,19 +9,42 @@ class NewFurniture extends React.Component {
   state = {
     activePage: 0,
     activeCategory: 'bed',
+    activeFade: false,
   };
 
   handlePageChange(newPage) {
-    this.setState({ activePage: newPage });
+    this.setState({
+      activePage: newPage,
+      activeFade: true,
+    });
+    if (this.state.activeFade === false) {
+      setTimeout(
+        function() {
+          this.setState({ activeFade: false });
+        }.bind(this),
+        1000
+      );
+    }
   }
 
   handleCategoryChange(newCategory) {
-    this.setState({ activeCategory: newCategory });
+    this.setState({
+      activeCategory: newCategory,
+      activeFade: true,
+    });
+    if (this.state.activeFade === false) {
+      setTimeout(
+        function() {
+          this.setState({ activeFade: false });
+        }.bind(this),
+        1000
+      );
+    }
   }
 
   render() {
     const { categories, products, assignFavourite, assignCompare } = this.props;
-    const { activeCategory, activePage } = this.state;
+    const { activeCategory, activePage, activeFade } = this.state;
 
     const categoryProducts = products.filter(item => item.category === activeCategory);
     const pagesCount = Math.ceil(categoryProducts.length / 8);
@@ -71,7 +94,7 @@ class NewFurniture extends React.Component {
               </div>
             </div>
           </div>
-          <div className='row'>
+          <div className={`row ${activeFade ? styles.fadeIn : styles.fadeOut}`}>
             {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
               <div key={item.id} className='col col-12 col-md-4 col-lg-3'>
                 <ProductBox
@@ -112,6 +135,7 @@ NewFurniture.propTypes = {
   ),
   assignFavourite: PropTypes.func,
   assignCompare: PropTypes.func,
+  activeFade: PropTypes.bool,
 };
 
 NewFurniture.defaultProps = {
